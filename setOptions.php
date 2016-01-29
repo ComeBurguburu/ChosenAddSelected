4108eee414240d73d973712b47a31eb72ae525c7
CômeBurguburu <come.burguburu@gmail.com>
2016-01-29 20:20:58 +0100

    Update setOptions.php

--- setOptions.php
+++ setOptions.php
@@ -9,13 +9,13 @@ include('base.php');
 $base->exec('INSERT INTO produit (droit) values ("'.$value_in.'")') or die(print_r($base->errorInfo())); 
 $response = $base->query('SELECT droit,id FROM produit ORDER BY id ASC') or die(print_r($base->errorInfo())); 
  
-$uniq=new Array();
+$uniq=Array();
     while($data = $response->fetch() ) 
   {
         if(in_array($data["droit"],$uniq)){
             continue;
         }
         $uniq[]=$data['droit'];
-      echo "<option value=\"".$data["id"]."\"".($data["droit"]===$value_in?"selected":"").">".$data["droit"]."</option>";
+      echo "<option value=\"".$data["id"]."\"".($data["droit"]==$value_in?"selected":"").">".$data["droit"]."</option>";
   }
 ?>
